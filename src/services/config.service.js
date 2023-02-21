e4488c6040f49c2917b302e1cf17aa94082d31c2
Ben Butcher <a-sterix@hotmail.com>
2023-02-21 13:28:45 +0000

    api added

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapibb-env.eba-qptzi2fn.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
